850a2ef844d67d58779f316e9290a98ebe33b1b5
ankur-agg1987 <40783014+ankur-agg1987@users.noreply.github.com>
2020-07-14 12:53:23 +0530

    Third update by Ankur adding onCreateOptionMenu function to ThirdActivity.java

--- app/src/main/java/com/example/myappbaroptionmenu/ThirdActivity.java
+++ app/src/main/java/com/example/myappbaroptionmenu/ThirdActivity.java
@@ -3,6 +3,7 @@ package com.example.myappbaroptionmenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        System.out.println("Hello");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
